4a4dcef02c4aab309bd3253803f23f2707e55bd4
Benjamin Patch <ben@benjaminpatch.com>
2025-05-01 16:39:58 -0700

    Create the 'login' view

--- app/auth.py
+++ app/auth.py
@@ -9,6 +9,7 @@ from flaskr.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
+# register route and view
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
   if request.method == 'POST':
@@ -37,3 +38,31 @@ def register():
     flash(error)
 
   return render_template('auth/register.html')
+
+# login route and view
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+  if request.method == 'POST':
+    username = request.form['username']
+    password = request.form['password']
+    db = get_db()
+    error = None
+    user = db.execute(
+      'SELECT * FROM user WHERE username = ?', (username,)
+    ).fetchone()
+
+    if user is None:
+      error = 'Incorrect username.'
+    elif not check_password_hash(user['password'], password):
+      error = 'Incorrect password.'
+    
+    if error is None:
+      session.clear()
+      session['user_id'] = user['id']
+      return redirect(url_for('index'))
+    
+    flash(error)
+
+  return render_template('auth/login.html')
+
+# continue with @bp.before_app_request route
