06eb09603be3781a9750b05a7fc769a7e7355b4e
Benjamin Patch <ben@benjaminpatch.com>
2025-05-01 19:55:22 -0700

    Check if a user id is stored in the session

--- app/auth.py
+++ app/auth.py
@@ -65,4 +65,15 @@ def login():
 
   return render_template('auth/login.html')
 
-# continue with @bp.before_app_request route
+# check if a user id is stored in the session
+# get that user’s data from the database
+@bp.before_app_request
+def load_logged_in_user():
+  user_id = session.get('user_id')
+
+  if user_id is None:
+    g.user = None
+  else:
+    g.user = get_db().execute(
+      'SELECT * FROM user WHERE id = ?', (user_id,)
+    ).fetchone()
